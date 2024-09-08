690ea44b719c434587667bd12f6026f06221097e
RaihanAhmadKusumardana <165549890+RaihanAhmadKusumardana@users.noreply.github.com>
2024-09-08 19:38:54 +0700

    Add files via upload
    Izin kang, untuk yang no 2 ada di file opeasi matematika sebagai interface

---
+++ tugas_pertemuan_2_no_1/pkg2/OperasiMatematika.java
@@ -0,0 +1,9 @@
+package tugas_pertemuan_2_no_1.pkg2;
+
+// Interface untuk operasi matematika
+public interface OperasiMatematika {
+    int pertambahan(int a, int b);
+    int pengurangan(int a, int b);
+    int perkalian(int a, int b);
+    int pembagian(int a, int b);
+}
