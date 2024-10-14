20abc38d41b30f96d9bec3a39ec995339b90ae4b
RaihanAhmadKusumardana <165549890+RaihanAhmadKusumardana@users.noreply.github.com>
2024-10-14 18:13:21 +0700

    Delete tugas_pertemuan_2_no_1/pkg2 directory

--- tugas_pertemuan_2_no_1/pkg2/OperasiMatematika.java
+++
@@ -1,9 +0,0 @@
-package tugas_pertemuan_2_no_1.pkg2;
-
-// Interface untuk operasi matematika
-public interface OperasiMatematika {
-    int pertambahan(int a, int b);
-    int pengurangan(int a, int b);
-    int perkalian(int a, int b);
-    int pembagian(int a, int b);
-}
