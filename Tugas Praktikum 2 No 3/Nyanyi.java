e8fa6f9fc87b85066aca4982c3173ceb3dd093c1
RaihanAhmadKusumardana <165549890+RaihanAhmadKusumardana@users.noreply.github.com>
2024-10-14 18:17:13 +0700

    Add files via upload

---
+++ Tugas Praktikum 2 No 3/Nyanyi.java
@@ -0,0 +1,13 @@
+package latihan_praktikum2_no3;
+
+public class Nyanyi {
+    // Method utama untuk menjalankan program
+    public static void main(String[] args) {
+    LaguBingo laguBingo = new LaguBingo(
+        "There was a farmer who had a dog,", 
+        "And Bingo was his name-o.", 
+        "And Bingo was his name-o.");
+    laguBingo.cetakLagu();
+    
+    }
+}
