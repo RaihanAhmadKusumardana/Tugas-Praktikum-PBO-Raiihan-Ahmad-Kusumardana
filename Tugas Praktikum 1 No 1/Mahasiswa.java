8cd55fa48d2ce593c949feda4885f9520f0d6e16
RaihanAhmadKusumardana <165549890+RaihanAhmadKusumardana@users.noreply.github.com>
2024-10-14 18:07:14 +0700

    Add files via upload

---
+++ Tugas Praktikum 1 No 1/Mahasiswa.java
@@ -0,0 +1,15 @@
+package tugas_pertemuan1_no1;
+
+public class Mahasiswa {
+    public void membaca(){
+        System.out.println("Mahasiswa sedang membaca");
+    }
+
+    public void nyontek(){
+        System.out.println("Mahasiswa sedang menyontek");
+    }
+
+    public void modifikasi(){
+        System.out.println("Mahasiswa sedang memodifikasi");
+    }
+}
---
+++ Tugas Praktikum 1 No 1/mahasiswaBeraksi.java
@@ -0,0 +1,11 @@
+package tugas_pertemuan1_no1;
+
+public class mahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.membaca();
+        mahasiswa.nyontek();
+        mahasiswa.modifikasi();
+    }
+}
+
