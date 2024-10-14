c4474a5c4a9fd03cf2b26a60a7afbd5152eafea6
RaihanAhmadKusumardana <165549890+RaihanAhmadKusumardana@users.noreply.github.com>
2024-10-15 01:43:28 +0700

    LaguAnakAyam.java

--- Tugas Praktikum 6 No 1&2/LaguAnakAyam.java
+++ Tugas Praktikum 6 No 1&2/LaguAnakAyam.java
@@ -1,6 +1,6 @@
 public class LaguAnakAyam {
     public static void main(String[] args) {
-        int n = 10; // Kamu bisa mengganti nilai N sesuai kebutuhan
+        int n = 10;
         for (int i = n; i > 0; i--) {
             if (i == 1) {
                 System.out.println("Anak ayam turun " + i + ", mati satu tinggal induknya.");
